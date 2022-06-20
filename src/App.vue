b8157f6a60036c74eb33904428f7efe6e8542abb
felicetto13 <felice.laterza93@icloud.com>
2022-06-20 14:14:40 +0200

    bonus part completed

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
+    <JumbotronComponent></JumbotronComponent>
     <TitlePage></TitlePage>
   </div>
 </template>
 
 <script>
 import TitlePage from './components/TitlePage.vue'
+import JumbotronComponent from './components/JumbotronComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    TitlePage
+    TitlePage, JumbotronComponent
   }
 }
 </script>
 
 <style lang="scss">
+body{
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/JumbotronComponent.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="container py-5 text-center">
+    <img src="./../assets/vue_jumbo.png" class="image-jumbo" alt="Jumbotron">
+</div>
+</template>
+
+
+<style scoped>
+.image-jumbo {
+    width: 100%;
+    height: 70vh;
+    object-fit: fill;
+}
+</style>
